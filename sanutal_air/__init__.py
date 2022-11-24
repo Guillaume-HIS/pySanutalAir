500cdeb0fb0d241c29c4837312a4084106f18346
Guillaume-HIS <=>
2022-11-24 01:06:04 +0100

    Update speed and filter/frost

--- sanutal_air/__init__.py
+++ sanutal_air/__init__.py
@@ -8,7 +8,54 @@ class Ventilation(object):
     def __init__(self, host, name="Sanutal_Air"):
         self._name = name
         self._host = host
-        self._is_on = None
+        self._state = None
         self._speed = None
         self._frost_active = None
         self._filter_reset = None
+    
+    def update(self):
+        ret = requests.get("http://" + self._host + "/")
+        self.fetch_speed(self, ret)
+        self.fetch_frost_filter(self, ret)
+    
+    def fetch_speed(self, req_get):
+        level = 0
+
+        if "B1" in req_get.text[-130:]:
+            level = 1
+        if "B2" in req_get.text[-130:]:
+            level = 2
+        if "B3" in req_get.text[-130:]:
+            level = 3
+        if "B4" in req_get.text[-130:]:
+            level = 4
+        
+        if level == 4 : return 100
+
+        substring = f"document.getElementById(\"R{level}\").value="
+        speed_index = req_get.text.rfind(substring)
+        self._speed = int(req_get.text[len(substring) + speed_index : len(substring) + speed_index + 3].replace(";", "").replace("<", ""))
+
+        if self._speed == 0:
+            self._state = "on"
+        else:
+            self._state = "off"
+
+    def fetch_frost_filter(self, req_get):
+        # Frost Active sensor
+        substring = f"document.getElementById(\"D2\").style.backgroundColor=\""
+        speed_index = req_get.text.rfind(substring)
+        color_value = req_get.text[len(substring) + speed_index : len(substring) + speed_index + 7]
+        if color_value == "#ffffff":
+            self._frost_active = False
+        else:
+            self._frost_active = True
+        
+        # Filter Reset sensor
+        substring = f"document.getElementById(\"D3\").style.backgroundColor=\""
+        speed_index = req_get.text.rfind(substring)
+        color_value = req_get.text[len(substring) + speed_index : len(substring) + speed_index + 7]
+        if color_value == "#ffffff":
+            self._filter_reset = False
+        else:
+            self._filter_reset = True
